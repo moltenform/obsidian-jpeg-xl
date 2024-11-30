d9d3a70a24de81983396054ce9f6ae3e40715795
moltenform.com(Ben Fisher) <amoltenform@gmail.com>
2024-11-29 20:45:18 -0800

    sample plugin works

---
+++ deploy-for-testing.sh
@@ -0,0 +1,5 @@
+cp main.js ../parent/.obsidian/plugins/obsidian-jpeg-xl
+cp manifest.json ../parent/.obsidian/plugins/obsidian-jpeg-xl
+cp styles.css ../parent/.obsidian/plugins/obsidian-jpeg-xl
+cp data.json ../parent/.obsidian/plugins/obsidian-jpeg-xl
+
